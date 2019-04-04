7839efa2ab8f2746905bb533126e0defa36877f7
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-04 19:43:47 +0000

    current construction of cli

--- lib/pcd-calendar/cli.rb
+++ lib/pcd-calendar/cli.rb
@@ -2,8 +2,33 @@
 
 class PCDCalendar::CLI
 
+  MONTHS = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
     def call
-      puts "PCD CALENDAR!!!"
+      main_menu
     end
+
+    def main_menu
+      input = nil
+
+      while input != "exit"
+        puts "What month would you like to view events?"
+        puts "(type \"exit\" to exit program)"
+        puts "Enter number or month:"
+        MONTHS.each.with_index(1) do |month, i|
+          puts "#{i}. #{month}"
+        end
+
+        input = gets.strip
+
+        if (1..12).include?(input.to_i) || MONTHS.include?(input)
+          MONTHS.include?(input) ? month = MONTH.index(input) : month = input
+        end
+
+          case input
+          when "quick view"
+            upcoming_events
+          when "1"
+
 
 end
