d701370997e2feb4f68fb4dbf209869b1c059bf1
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-05 18:48:16 +0000

    finished scrape_calendar_page

--- lib/pcd-calendar/scraper.rb
+++ lib/pcd-calendar/scraper.rb
@@ -10,11 +10,20 @@ class PCDCalendar::Scraper
   def self.scrape_calendar_page(url)
     html = open(url)
     doc = Nokogiri::HTML(html)
+    events_hash_array = []
+    event_hash = {}
 
     doc.css(".tribe-events-month-event-title").each do |event|
-      event_url = event.css("a").attr("href").value
-      self.scrape_event_page(event_url)
+      event_hash = {
+                name: event.text,
+                url:  event.css("a").attr("href").value
+              }
+      # binding.pry
+      events_hash_array << event_hash
+      # binding.pry
+      # self.scrape_event_page(event_url)
     end
+    binding.pry
   end
 
   def self.scrape_event_page(url)
@@ -28,16 +37,16 @@ class PCDCalendar::Scraper
 
 
     event_hash = {
-                  title:   doc.css(".tribe-events-single-event-title").text.strip
-                  date:    doc.css("abbr").attr("title").value
-                  venue:   doc.css(".tribe-venue").text.strip
-                  address: doc.css("span .tribe-street-address").text.strip
-                  city:    doc.css("span .tribe-locality").text.strip
-                  state:   doc.css("span .tribe-region").text.strip
+                  title:   doc.css(".tribe-events-single-event-title").text.strip,
+                  date:    doc.css("abbr").attr("title").value,
+                  venue:   doc.css(".tribe-venue").text.strip,
+                  address: doc.css("span .tribe-street-address").text.strip,
+                  city:    doc.css("span .tribe-locality").text.strip,
+                  state:   doc.css("span .tribe-region").text.strip,
                   zip:     doc.css("span .tribe-postal-code").text.strip
                   }
 
-      if is_recurring?(doc) # selectors differ when events are recurring vs. non-recurring
+      if self.is_recurring?(doc) # selectors differ when events are recurring vs. non-recurring
         event_hash[:time] = doc.css(".tribe-recurring-event-time").text
       else
         event_time[:time] = doc.css(".tribe-events-start-time").text
@@ -45,25 +54,24 @@ class PCDCalendar::Scraper
     #^^^^^^ end event_hash creation
 
     group_hash = {
-                  type:  doc.css("p strong").text.strip
+                  name:  doc.css("p strong").text.strip,
                   phone: doc.css(".tribe-venue-tel").text.strip
+                }
       if doc.css(".tribe-events-single-event-description").css("a")
         group_hash[:page] = doc.css(".tribe-events-single-event-description").css("a").attr("href").value
       end
 
       doc.css(".tribe-events-single-event-description").text.split("\n").each do |line|
         if line.include?("Email")
-          # binding.pry
           group_hash[:email] = line.split(":").last.strip
-          binding.pry
         end
       end
     #^^^^^^^ end group hash creation
-  end
+    # binding.pry
+  end # end self.scrape_event_page
 
-  binding.pry
-end
-
-  def is_recurring?(doc)
+  def self.is_recurring?(doc)
     doc.css("div .recurringinfo").text ? true : false
   end
+
+end
